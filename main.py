c7d2299b2dbf09e09d47b4ceec1444b1739f0085
Datascifer <150779158+Datascifer@users.noreply.github.com>
2025-01-06 00:12:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -144,4 +144,4 @@ def get_all_passwords():
 
 if __name__ == "__main__":
     # For local testing
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    main.run(debug=True, host="0.0.0.0", port=5000)
